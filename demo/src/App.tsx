b1f5938d19e007fded117045bd81db57488e3a33
Pankaj Patel <pankaj.patel@abtasty.com>
2023-01-04 11:10:47 +0100

    updated-dependencies

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Steps from './steps';
-import './App.css';
+import React from "react";
+import Steps from "./steps";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Please select your appointment time:
-        </p>
-        {Steps.map((Step, index) => <Step key={index} />)}
+        <p>Please select your appointment time:</p>
+        {Steps.map((Step, index) => (
+          <Step key={index} />
+        ))}
       </header>
     </div>
   );
